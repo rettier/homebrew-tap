d2134e1ff5dea26f2e2cf23884cada6352e32e4f
Philipp Reitter <p.reitter@gmail.com>
2019-01-05 14:16:41 +0100

    corrected download path

--- Formula/c.rb
+++ Formula/c.rb
@@ -1,8 +1,8 @@
 class C < Formula
   desc "The C Macro"
   homepage "https://github.com/rettier/c"
-  url "https://github.com/rettier/c/releases/download/v1.0.8/c_1.0.8.deb"
-  sha256 "a876eaa67789588d14a3142d2badc68479f1d96d90483fe27eeba45acbe0eb54"
+  url "https://github.com/rettier/c/releases/download/v1.0.8/c_1.0.8.tar.gz"
+  sha256 "719c2e9fe4b87502eb5c698f86db1fbcb5ecbe88107e1057f4ef2f233f8fe2bc"
 
   def install
     bin.install "c"
