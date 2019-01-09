91479d93d7658d36cb506d74094d10f56d76e7e7
rettier <rettier@users.noreply.github.com>
2019-01-09 18:14:28 +0100

    update to 1.0.9

--- Formula/c.rb
+++ Formula/c.rb
@@ -1,8 +1,8 @@
 class C < Formula
   desc "The C Macro"
   homepage "https://github.com/rettier/c"
-  url "https://github.com/rettier/c/releases/download/v1.0.8/c_1.0.8.tar.gz"
-  sha256 "719c2e9fe4b87502eb5c698f86db1fbcb5ecbe88107e1057f4ef2f233f8fe2bc"
+  url "https://github.com/rettier/c/releases/download/v1.0.9/c_1.0.9.tar.gz"
+  sha256 "2f7f3dee638fd0047ee6efb59f7002a74f8b29b6adc6e07e02b4962f3465c21a"
 
   def install
     bin.install "c"
